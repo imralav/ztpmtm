7035bf4549ea72560eea9a5f0f0139bb1135a171
imralav <imralav@gmail.com>
2014-11-29 16:50:41 +0100

    Quickfix.

--- html/src/pl/edu/pb/wi/ztpmtm/client/HtmlLauncher.java
+++ html/src/pl/edu/pb/wi/ztpmtm/client/HtmlLauncher.java
@@ -6,14 +6,15 @@ import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import pl.edu.pb.wi.ztpmtm.Core;
 
 public class HtmlLauncher extends GwtApplication {
+	private final static int DEFAULT_WIDTH = 1024, DEFAULT_HEIGHT = 512;
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
+	@Override
+	public GwtApplicationConfiguration getConfig() {
+		return new GwtApplicationConfiguration(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
 
-        @Override
-        public ApplicationListener getApplicationListener () {
-                return new Core();
-        }
+	@Override
+	public ApplicationListener getApplicationListener() {
+		return Core.INSTANCE;
+	}
 }
