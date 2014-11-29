f83ca0c9727c1583968bcfcd82164170158c2a65
imralav <imralav@gmail.com>
2014-11-29 18:29:03 +0100

    Testy 3

--- core/src/pl/edu/pb/wi/ztpmtm/Core.java
+++ core/src/pl/edu/pb/wi/ztpmtm/Core.java
@@ -34,7 +34,7 @@ public enum Core implements ApplicationListener {
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(4, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		InterfaceManager.INSTANCE.render(Gdx.graphics.getDeltaTime());
 	}
