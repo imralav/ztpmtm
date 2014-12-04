893b0c37e1afa29ce0750224de5c1d1cabef5d59
imralav <imralav@gmail.com>
2014-12-04 16:51:47 +0100

    Wali w brancha czy w mastera?

--- desktop/src/pl/edu/pb/wi/ztpmtm/desktop/DesktopLauncher.java
+++ desktop/src/pl/edu/pb/wi/ztpmtm/desktop/DesktopLauncher.java
@@ -13,7 +13,7 @@ import com.badlogic.gdx.graphics.Color;
  * @author MJ */
 public class DesktopLauncher {
 	private static final String KONFIGURATS = "Konfigurats";
-	private final static int DEFAULT_WIDTH = 1024, DEFAULT_HEIGHT = 512;
+	private final static int DEFAULT_WIDTH = 1024/2, DEFAULT_HEIGHT = 512/2;
 
 	public static void main(String[] arg) {
 		new LwjglApplication(Core.INSTANCE, getDefaultConfiguration());
