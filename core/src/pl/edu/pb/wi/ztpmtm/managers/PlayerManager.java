1351a2cd22ffb9d49df326a3cd83a30a60e66e8f
marsza <marsza@czapa>
2014-12-20 14:01:32 +0100

    nie wiem co

---
+++ core/src/pl/edu/pb/wi/ztpmtm/managers/PlayerManager.java
@@ -0,0 +1,22 @@
+package pl.edu.pb.wi.ztpmtm.managers;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+public enum PlayerManager {
+	INSTANCE;
+
+	PlayerManager() {
+	}
+
+	public void loadAssetPlayer() {
+		// TODO Auto-generated method stub
+	}
+
+	public void dispose() {
+	}
+
+}
--- desktop/src/pl/edu/pb/wi/ztpmtm/desktop/DesktopLauncher.java
+++ desktop/src/pl/edu/pb/wi/ztpmtm/desktop/DesktopLauncher.java
@@ -9,31 +9,32 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
 
 /** Launches desktop application.
- * 
+ *
  * @author MJ */
 public class DesktopLauncher {
 	private static final String KONFIGURATS = "Konfigurats";
 	private final static int DEFAULT_WIDTH = 400, DEFAULT_HEIGHT = 600;
 
-	public static void main(String[] arg) {
+	public static void main(final String[] arg) {
 		new LwjglApplication(Core.INSTANCE, getDefaultConfiguration());
 	}
 
 	private final static LwjglApplicationConfiguration getDefaultConfiguration() {
-		LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+		final LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
 		configuration.title = KONFIGURATS;
 		configuration.initialBackgroundColor = new Color(0.0f, 0.0f, 0.1f, 1f);
 		setSize(configuration);
 		addIcons(configuration);
+		// TODO disable resize
 		return configuration;
 	}
 
-	private static void setSize(LwjglApplicationConfiguration configuration) {
+	private static void setSize(final LwjglApplicationConfiguration configuration) {
 		configuration.width = DEFAULT_WIDTH;
 		configuration.height = DEFAULT_HEIGHT;
 	}
 
-	private static void addIcons(LwjglApplicationConfiguration configuration) {
+	private static void addIcons(final LwjglApplicationConfiguration configuration) {
 		configuration.addIcon(Asset.GAME_ICON_BIG.getPath(), FileType.Internal);
 		configuration.addIcon(Asset.GAME_ICON_MEDIUM.getPath(), FileType.Internal);
 		configuration.addIcon(Asset.GAME_ICON_SMALL.getPath(), FileType.Internal);
